041810964feb70fce32dc277394763f14ea4de8d
Shinya Abe <s-abe@details.co.jp>
2023-05-07 11:29:59 +0900

    add inputと音楽用で切り替えできるように対応

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 import "./style.scss"
-import { gsap } from "gsap"
+import GUI from "lil-gui"
 
 const FFT_SIZE = 256
 
 class Main {
   isPlaying = false
+  isFreqency = false
+  isTimeDomain = true
+  isMix = false
+  isInput = false
   constructor() {
     this.elemSetup()
-    // this.sample.addEventListener('loadstart', this.audioSetup.bind(this))
-    this.audioSetup()
+    this.commonAudioSetup()
+    this.musicAudioSetup()
+    // this.inputAudioSetup()
+    this.addGUI()
     this.canvasSetup()
-    // this.onTick()
   }
 
   elemSetup() {
@@ -27,20 +32,34 @@ class Main {
     this.sample = document.querySelector("#sample")
   }
 
-  audioSetup() {
-    console.log("Audio setup")
-    // AudioContext
+  commonAudioSetup() {
     this.audioCtx = new AudioContext()
-    this.track = this.audioCtx.createMediaElementSource(this.sample)
-
     // AudioAnalyzer
     this.analyser = this.audioCtx.createAnalyser()
     this.analyser.fftSize = FFT_SIZE
-    this.bufferLength = this.analyser.frequencyBinCount
-    this.dataArray = new Uint8Array(this.bufferLength)
-    // this.analyser.getByteTimeDomainData(this.dataArray);
+
+    // Freq
+    this.freqLength = this.analyser.frequencyBinCount
+    this.freqArray = new Uint8Array(this.freqLength)
+
+    // TimeDomain
+    this.tdLength = this.analyser.fftSize
+    this.tdArray = new Float32Array(this.tdLength)
+  }
+
+  musicAudioSetup() {
+    console.log("Audio setup")
+    this.track =
+      this.track || this.audioCtx.createMediaElementSource(this.sample)
     this.analyser.connect(this.audioCtx.destination)
     this.track.connect(this.analyser)
+  }
+
+  async inputAudioSetup() {
+    console.log("InputAudio setup")
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true })
+    this.input = this.input || this.audioCtx.createMediaStreamSource(stream)
+    this.input.connect(this.analyser)
   }
 
   canvasSetup() {
@@ -64,6 +83,38 @@ class Main {
     this.canvasCtx.lineWidth = 1
   }
 
+  addGUI() {
+    this.gui = new GUI()
+    const params = {
+      Frequency: this.isFreqency,
+      TimeDomain: this.isTimeDomain,
+      isMix: this.isMix,
+      isInput: this.isInput,
+    }
+    this.gui
+      .add(params, "Frequency")
+      .onChange((value) => (this.isFreqency = value))
+    this.gui
+      .add(params, "TimeDomain")
+      .onChange((value) => (this.isTimeDomain = value))
+    this.gui.add(params, "isMix").onChange((value) => (this.isMix = value))
+    this.gui.add(params, "isInput").onChange((value) => {
+      if (value) {
+        this.inputAudioSetup()
+        this.isPlaying = true
+        this.sample.pause()
+        this.onTick()
+        this.playBtn.style.display = "none"
+        this.stopBtn.style.display = "none"
+      } else {
+        this.musicAudioSetup()
+        this.isPlaying = false
+        this.playBtn.style.display = "inline-block"
+        this.stopBtn.style.display = "inline-block"
+      }
+    })
+  }
+
   play() {
     // 一時停止されたオーディオコンテキストの時間の流れを再開
     if (this.audioCtx.state === "suspended") {
@@ -81,77 +132,139 @@ class Main {
 
   onTick() {
     if (this.isPlaying) requestAnimationFrame(this.onTick.bind(this))
-    this.analyser.getByteFrequencyData(this.dataArray)
-    console.log(this.dataArray)
     const { width, height } = this.canvas
     this.canvasCtx.clearRect(0, 0, width, height)
-    const slideWidth = (width * 0.5) / this.bufferLength
-    // xスタート位置
-    let x = width / 2
-    let _x = width / 2
     this.canvasCtx.beginPath()
-    for (let i = 0; i < this.bufferLength; i++) {
-      const d = this.dataArray[i]
-      const v = d / 300
-      // const y = height / 2 + v * (height / 2)
-      // const _y = height / 2 - v * (height / 2)
-      let y = 0
-      if (i % 2 === 0 && v !== 0) {
-        y = height / 2 - v * (height / 2)
-      } else {
-        y = height / 2 + v * (height / 2)
+
+    // フレケンシードメイン（周波数領域）
+    if (
+      (this.isFreqency && !this.isMix) ||
+      (this.isFreqency && !this.isTimeDomain)
+    ) {
+      this.analyser.getByteFrequencyData(this.freqArray)
+      console.log(this.freqArray)
+      const freqX_shift = (width * 0.5) / this.freqLength
+      // xスタート位置
+      let x = width / 2
+      let _x = width / 2
+      this.canvasCtx.beginPath()
+      for (let i = 0; i < this.freqLength; i++) {
+        const d = this.freqArray[i]
+        const v = d / 256
+        let y = 0
+        if (i % 2 === 0 && v !== 0) {
+          y = height / 2 - v * (height / 2)
+        } else {
+          y = height / 2 + v * (height / 2)
+        }
+
+        if (i === 0) {
+          this.canvasCtx.moveTo(x, y)
+        } else {
+          this.canvasCtx.lineTo(x, y)
+        }
+        x += freqX_shift
       }
 
-      if (i === 0) {
-        this.canvasCtx.moveTo(x, y)
-      } else {
-        this.canvasCtx.lineTo(x, y)
+      for (let i = 0; i < this.freqLength; i++) {
+        const d = this.freqArray[i]
+        const v = d / 256
+        let y = 0
+        if (i % 2 === 0 && v !== 0) {
+          y = height / 2 - v * (height / 2)
+        } else {
+          y = height / 2 + v * (height / 2)
+        }
+
+        if (i === 0) {
+          this.canvasCtx.moveTo(_x, y)
+        } else {
+          this.canvasCtx.lineTo(_x, y)
+        }
+        _x -= freqX_shift
       }
-      x += slideWidth
-      // _x -= slideWidth
     }
 
-    for (let i = 0; i < this.bufferLength; i++) {
-      const d = this.dataArray[i]
-      const v = d / 300
-      let y = 0
-      if (i % 2 === 0 && v !== 0) {
-        y = height / 2 - v * (height / 2)
-      } else {
-        y = height / 2 + v * (height / 2)
+    // タイムドメイン（時間領域）
+    if (
+      (this.isTimeDomain && !this.isMix) ||
+      (this.isTimeDomain && !this.isFreqency)
+    ) {
+      const tdX_shift = width / this.tdLength
+      this.analyser.getFloatTimeDomainData(this.tdArray)
+      let tdX = 0
+      for (let i = 0; i < this.tdLength; i++) {
+        const d = this.tdArray[i]
+        const v = d / 2
+        const y = height / 2 + v * height
+        if (i === 0) {
+          this.canvasCtx.moveTo(tdX, y)
+        } else {
+          this.canvasCtx.lineTo(tdX, y)
+        }
+        tdX += tdX_shift
       }
-      // const y = height / 2 + v * (height / 2)
-      // const _y = height / 2 - v * (height / 2)
+    }
 
-      if (i === 0) {
-        this.canvasCtx.moveTo(_x, y)
-      } else {
-        this.canvasCtx.lineTo(_x, y)
+    // Mixed
+    if (this.isFreqency && this.isTimeDomain && this.isMix) {
+      this.analyser.getByteFrequencyData(this.freqArray)
+      this.analyser.getFloatTimeDomainData(this.tdArray)
+      const x_shift = (width * 0.5) / this.freqLength
+      let x = width / 2
+      let _x = width / 2
+
+      for (let i = 0; i < this.freqLength; i++) {
+        // Freq
+        const freq_d = this.freqArray[i]
+        const freq_v = freq_d / 256
+        let freq_y = 0
+        if (i % 2 === 0 && freq_v !== 0) {
+          freq_y = height / 2 - freq_v * (height / 2)
+        } else {
+          freq_y = height / 2 + freq_v * (height / 2)
+        }
+
+        // TimeDomain
+        const td_d = this.tdArray[this.freqLength + i]
+        const td_v = td_d / 2
+        const td_y = height / 2 + td_v * height
+
+        if (i === 0) {
+          this.canvasCtx.moveTo(x, freq_y * 0.5 + td_y * 0.5)
+        } else {
+          this.canvasCtx.lineTo(x, freq_y * 0.5 + td_y * 0.5)
+        }
+        x += x_shift
       }
-      // x += slideWidth
-      _x -= slideWidth
+
+      for (let i = 0; i < this.freqLength; i++) {
+        // Freq
+        const freq_d = this.freqArray[i]
+        const freq_v = freq_d / 256
+        let freq_y = 0
+        if (i % 2 === 0 && freq_v !== 0) {
+          freq_y = height / 2 - freq_v * (height / 2)
+        } else {
+          freq_y = height / 2 + freq_v * (height / 2)
+        }
+
+        // TimeDomain
+        const td_d = this.tdArray[this.freqLength - i]
+        const td_v = td_d / 2
+        const td_y = height / 2 + td_v * height
+        console.log({ freq_y, td_y })
+
+        if (i === 0) {
+          this.canvasCtx.moveTo(_x, freq_y * 0.5 + td_y * 0.5)
+        } else {
+          this.canvasCtx.lineTo(_x, freq_y * 0.5 + td_y * 0.5)
+        }
+        _x -= x_shift
+      }
     }
 
     this.canvasCtx.stroke()
-
-    // this.canvasCtx.moveTo(
-    //   i * ((width * 0.5) / length) + (length / 2) * (width / length),
-    //   height / 2 - (d / 255) * (height * 0.45)
-    // )
-    // this.canvasCtx.lineTo(
-    //   i * ((width * 0.5) / length) + (length / 2) * (width / length),
-    //   height / 2 + (d / 255) * (height * 0.45)
-    // )
-
-    // // 左右反転
-    // this.canvasCtx.moveTo(
-    //   -i * ((width * 0.5) / length) + (length / 2) * (width / length),
-    //   height / 2 - (d / 255) * (height * 0.45)
-    // )
-    // this.canvasCtx.lineTo(
-    //   -i * ((width * 0.5) / length) + (length / 2) * (width / length),
-    //   height / 2 + (d / 255) * (height * 0.45)
-    // )
   }
 }
 
